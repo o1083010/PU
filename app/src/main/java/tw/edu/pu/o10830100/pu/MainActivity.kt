676aeb5ee53dbb9dbdb5229706a327536cb1a4b4
o1083010 <o1083010@gm.pu.edu.tw>
2021-04-12 11:55:00 +0800

    全螢幕觸控及手勢識別2

--- app/src/main/java/tw/edu/pu/o10830100/pu/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o10830100/pu/MainActivity.kt
@@ -4,19 +4,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(),GestureDetector.OnGestureListener {
+class MainActivity : AppCompatActivity(),GestureDetector.OnGestureListener
+     {
 
     lateinit var gDetector: GestureDetector
+         var PictureNo:Int = 0  //目前顯示第幾張圖
+         var TotalPictures:Int = 0 //總共幾張圖片(假設僅顯示pu0-pu3)
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+
+         fun ShowPicture(){
+             /*
+             when (PictureNo){
+                 0 -> img.setImageResource(R.drawable.pu0)
+                 1 -> img.setImageResource(R.drawable.pu1)
+                 2 -> img.setImageResource(R.drawable.pu2)
+                 3 -> img.setImageResource(R.drawable.pu3)
+             }
+              */
+             var res:Int = getResources().getIdentifier("pu" + (PictureNo),
+                 "drawable", getPackageName())
+             img.setImageResource(res)
+
+             txv.text = PictureNo.toString()
+
+         }
+
+
+         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
             gDetector = GestureDetector(this, this)
 
-        }
+             var res:Int = -1
+             var countDrawables:Int = -1
+             while (res != 0) {
+                 countDrawables++;
+                 res = getResources().getIdentifier("pu" + (countDrawables),
+                     "drawable", getPackageName());
+             }
+             TotalPictures = countDrawables
+
+
+         }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         /*
@@ -36,18 +69,20 @@ class MainActivity : AppCompatActivity(),GestureDetector.OnGestureListener {
 
     override fun onShowPress(e: MotionEvent?) {
         //TODO("Not yet implemented")
-        txv.text = "按下後無後續動作"
+        //txv.text = "按下後無後續動作"
     }
 
     override fun onSingleTapUp(e: MotionEvent?): Boolean {
         //TODO("Not yet implemented")
-        txv.text = "短按"
+        //txv.text = "短按"
+        PictureNo = 0
+        ShowPicture()
         return true
     }
 
     override fun onDown(e: MotionEvent?): Boolean {
         //TODO("Not yet implemented")
-        txv.text = "按下"
+        //txv.text = "按下"
         return true
     }
 
@@ -58,9 +93,19 @@ class MainActivity : AppCompatActivity(),GestureDetector.OnGestureListener {
         velocityY: Float
     ): Boolean {
         //TODO("Not yet implemented")
-        txv.text = "快滑\nx1y1: " + e1.getX().toString() + ", " + e1.getY().toString() +
+        //txv.text = "快滑\nx1y1: " + e1.getX().toString() + ", " + e1.getY().toString() +
                 "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString() +
                 "\nX軸Y軸速度:" + velocityX.toString() + ", " +  velocityY.toString()
+        if (e1.getX() < e2.getX()){  //向右快滑
+            PictureNo++
+            if (PictureNo == TotalPictures) {PictureNo = 0}
+        }
+        else{     //向左快滑
+            PictureNo--;
+            if (PictureNo < 0) {PictureNo = TotalPictures - 1 }
+        }
+        ShowPicture()
+
         return true
     }
 
@@ -71,14 +116,16 @@ class MainActivity : AppCompatActivity(),GestureDetector.OnGestureListener {
         distanceY: Float
     ): Boolean {
         //TODO("Not yet implemented")
-        txv.text = "拖曳\nx1y1: " +  e1.getX().toString() + ", " + e1.getY().toString() +
+        //txv.text = "拖曳\nx1y1: " +  e1.getX().toString() + ", " + e1.getY().toString() +
                 "\nx2y2: " + e2.getX().toString() + ", " + e2.getY().toString()
         return true
     }
 
     override fun onLongPress(e: MotionEvent?) {
        // TODO("Not yet implemented")
-        txv.text = "長按"
+        //txv.text = "長按"
+        PictureNo = TotalPictures - 1
+        ShowPicture()
     }
 
 }
